de1273613d26911df68169c9d6daf36147d49192
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-01-19 01:24:50 +0600

    created an array and dynamically displayed in ul

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
-import _ from 'lodash';
 import './style.css';
 
-  let todoList = [{"description":"taking breakfast","completed":false,"index":'1'},
-  {"description":"taking breakfast","completed":false,"index":'2'},
-  {"description":"taking breakfast","completed":false,"index":'3'}
-  ]
-  
-  let todos = document.getElementsByClassName('todolist')[0]
-   todoList.forEach((todo)=>{
-    console.log(todo['description']) 
-    let task = document.createElement('li')
-    task.classList.add('task')
-    todos.appendChild(task)
-    task.innerHTML = todo['description']
-})
+const todoList = [{ description: 'taking breakfast', completed: false, index: '1' },
+  { description: 'taking breakfast 3', completed: false, index: '3' },
+  { description: 'taking breakfast 2', completed: false, index: '2' },
+];
+
+const todos = document.getElementsByClassName('todolist')[0];
+todoList.forEach((todo) => {
+  const task = document.createElement('li');
+  task.classList.add('task');
+  task.id = todo.index;
+  todos.appendChild(task);
+  task.innerHTML = todo.description;
+});
+
+const sortList = () => {
+  let i; let switching; let b; let
+    shouldSwitch;
+  switching = true;
+  while (switching) {
+    switching = false;
+    b = todos.getElementsByTagName('li');
+    for (i = 0; i < (b.length - 1); i += 1) {
+      shouldSwitch = false;
+      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      b[i].parentNode.insertBefore(b[i + 1], b[i]);
+      switching = true;
+    }
+  }
+};
+
+sortList();
