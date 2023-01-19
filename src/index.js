74c79cc28e0312a035451ee4550507e299ba6ade
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-01-20 03:35:15 +0600

    added addTask removeTask editTask functions

--- src/index.js
+++ src/index.js
@@ -1,18 +1,72 @@
 import './style.css';
+import TodoList from './modules/TodoList.js';
 
-const todoList = [{ description: 'taking breakfast', completed: false, index: '1' },
-  { description: 'taking breakfast 3', completed: false, index: '3' },
-  { description: 'taking breakfast 2', completed: false, index: '2' },
-];
+let todolist = [];
+if (JSON.parse(localStorage.getItem('todolist'))) {
+  todolist = JSON.parse(localStorage.getItem('todolist')).todolist;
+}
+const latestTodoList = new TodoList(todolist);
 
 const todos = document.getElementsByClassName('todolist')[0];
 
-const sortedTodoList = todoList.sort((a, b) => a.index - b.index);
+const sortedTodoList = todolist.sort((a, b) => a.index - b.index);
 
 sortedTodoList.forEach((todo) => {
   const task = document.createElement('li');
   task.classList.add('task');
   task.id = todo.index;
+  task.innerHTML = `<input type="checkbox" name="${todo.index}"><label id = "${todo.index}" for="${todo.index}">${todo.description}</label><div class="remove-button"><i class='fa fa-trash'></i><div>`;
   todos.appendChild(task);
-  task.innerHTML = todo.description;
 });
+
+const enterButton = document.getElementById('submit-new-item');
+
+const addTask = (e) => {
+  e.preventDefault();
+  const inputForm = document.getElementById('new-item');
+  if (inputForm.value) {
+    const description = inputForm.value.trim();
+    const index = todolist.length + 1;
+    latestTodoList.addTask(description, index);
+    localStorage.setItem('todolist', JSON.stringify(latestTodoList));
+    inputForm.value = '';
+  }
+  window.location.reload();
+};
+
+enterButton.addEventListener('click', addTask);
+
+const editButton = document.querySelectorAll('.task');
+editButton.forEach((elm) => {
+  const element = elm.children[1];
+  element.addEventListener('click', () => {
+    element.contentEditable = true;
+    element.focus();
+  });
+
+  element.addEventListener('focusout', () => {
+    if (element.innerHTML) {
+      latestTodoList.editTask(element.innerHTML, element.id);
+      localStorage.setItem('todolist', JSON.stringify(latestTodoList));
+      element.contentEditable = false;
+    }
+  });
+  element.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter' && element.innerHTML) {
+      latestTodoList.editTask(element.innerHTML, element.id);
+      localStorage.setItem('todolist', JSON.stringify(latestTodoList));
+      element.contentEditable = false;
+    }
+  });
+});
+
+const removeButton = document.querySelectorAll('.remove-button');
+
+const removeTask = (e) => {
+  const index = e.target.parentNode.id;
+  latestTodoList.removeTask(index);
+  localStorage.setItem('todolist', JSON.stringify(latestTodoList));
+  window.location.reload();
+};
+
+removeButton.forEach((element) => element.addEventListener('click', removeTask));
---
+++ src/modules/TodoList.js
@@ -0,0 +1,34 @@
+import { eq } from 'lodash';
+import Todo from './Todo.js';
+
+export default class TodoList {
+  constructor(todolist) {
+    this.todolist = todolist;
+  }
+
+  addTask(description, index) {
+    const newTask = new Todo(description, index);
+    this.todolist.push(newTask);
+  }
+
+  removeTask(index) {
+    this.todolist.splice(index, 1);
+    const updatedTodoList = this.todolist.map((object) => {
+      if (object.index > index) {
+        return { ...object, index: object.index - 1 };
+      }
+      return object;
+    });
+    this.todolist = updatedTodoList;
+  }
+
+  editTask(description, index) {
+    const updatedTodoList = this.todolist.map((object) => {
+      if (parseInt(object.index) === parseInt(index)) {
+        return { ...object, description };
+      }
+      return object;
+    });
+    this.todolist = updatedTodoList;
+  }
+}
