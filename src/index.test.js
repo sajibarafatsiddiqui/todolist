8969fa2ca209305817f95a607d22555c604c536a
Jean Walner Alexis <jeanwalneralexis@yahoo.com>
2023-01-25 20:18:08 -0500

    Tried to use localStorageMock

---
+++ src/index.test.js
@@ -0,0 +1,26 @@
+const localStorageMock = require('localstorage-mock');
+
+console.log('mock: ' + localStorageMock);
+
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM('<ul id="todolist"></ul>');
+const { document } = dom.window;
+const myList = document.getElementById('todolist');
+
+function addItem(item) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    myList.appendChild(li);
+}
+
+// Test the add function
+addItem('item1');
+const listItems = myList.getElementsByTagName('li');
+
+expect(listItems[0].textContent).toBe('item1');
+
+describe('Test add item', () => {
+    test('should be equal to 1', () => {
+        expect(listItems.length).toBe(1);
+    });
+});
